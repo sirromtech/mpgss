05f2e5307232aa6020edf387c2557ecda2ac2d9b
sirromtech <sirromenter@gmail.com>
2026-01-19 15:12:16 +1000

    Update settings.py
    comment out email

--- gss_scheme/settings.py
+++ gss_scheme/settings.py
@@ -167,14 +167,14 @@ STORAGES = {
 }
 
 # EMAIL
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env.int("EMAIL_PORT", default=587)
-EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
-EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_HOST = env("EMAIL_HOST")
+#EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+#EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+#EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
+#EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+#EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+#DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
 
 # INTERNATIONALIZATION
 LANGUAGE_CODE = "en-us"
