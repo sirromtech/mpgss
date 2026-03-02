5f193590bb531cff9528953554a1f7ee630528df
sirromtech <sirromenter@gmail.com>
2026-03-03 08:09:31 +1000

    Update apps.py

--- applications/apps.py
+++ applications/apps.py
@@ -6,7 +6,4 @@ class ApplicationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'applications'
 
-    def ready(self):
-        # Import signals so they are registered when Django starts
-        # Keep import local to avoid side effects at import time
-        from . import signals  # noqa: F401
+
